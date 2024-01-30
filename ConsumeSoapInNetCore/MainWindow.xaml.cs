c83e945f8edcd83a3604187abcf2ec83556c5105
Luis Gabriel Laurindo Mauricio <gah7583@gmail.com>
2024-01-29 23:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumeSoapInNetCore/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SoapService;
+using System.Windows;
+
+namespace ConsumeSoapInNetCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ISoapService _soapService = new SoapServiceClient(SoapServiceClient.EndpointConfiguration.BasicHttpBinding_ISoapService_soap);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            int number1 = Convert.ToInt32(tbNumber1.Text);
+            int number2 = Convert.ToInt32(tbNumber2.Text);
+            var response = await _soapService.SumAsync(new SumRequest()
+            {
+                Body = new SumRequestBody()
+                {
+                    num1 = number1,
+                    num2 = number2
+                }
+            });
+            lblResponse.Content = response.Body.SumResult;
+        }
+    }
+}
